7efe59662961088667b3472358faf36b0c675a76
Daniel Aschwanden <daniel.aschwanden@locatee.ch>
2022-07-18 13:03:53 +0200

    fix caching behavior

--- functions/rest/v1/[[path]].ts
+++ functions/rest/v1/[[path]].ts
@@ -1,20 +1,32 @@
 export async function onRequestGet({ request, env, params }) {
     const url = new URL(request.url);
-    const apiReq = new Request(`https://api.handball.ch${url.pathname}?${url.searchParams}`)
-    const res = await fetch(apiReq, {
-        cf: {
-            cacheTtl: 5,
-            cacheEverything: true,
-        },
-        headers: {
-            "Authorization": `Basic ${env.API_KEY}`,
-        }
-    });
+
+    const cache = caches.default;
+    const cacheKey = new Request(url.toString(), request);
+
+    let res = await cache.match(cacheKey);
+    if (!res) {
+        let apiReq = new Request(`https://api.handball.ch${url.pathname}?${url.searchParams}`)
+
+        res = await fetch(apiReq, {
+            cf: {
+                cacheTtl: 5,
+                cacheEverything: true,
+            },
+            headers: {
+                "Authorization": `Basic ${env.API_KEY}`,
+            }
+        });
+
+    }
 
     // Reconstruct the Response object to make its headers mutable.
     let response = new Response(res.body, res);
 
     // Set cache control headers to cache on browser for 25 minutes
     response.headers.set('Cache-Control', 'max-age=1500');
+
+    await cache.put(cacheKey, response.clone());
+
     return response;
 }

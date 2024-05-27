dc25e1cc22fd4bdf363c4d5d6b20b21fc4ae9ec8
Dani Aschwanden <nimdanitro@gmail.com>
2024-05-27 09:24:12 +0200

    fix setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,10 +4,10 @@ module.exports = function (app) {
   app.use(
     '/rest',
     createProxyMiddleware({
-      target: 'https://clubapi-test.handball.ch',
-      logLevel: 'info',
+      target: 'https://clubapi-test.handball.ch/rest ',
       changeOrigin: true,
       auth: atob(process.env.REACT_APP_API_AUTH_TOKEN),
+      logger: console,
     })
   );
 };

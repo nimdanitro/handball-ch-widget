5256e140de52b0989b30437bf697e8d44688bf0e
Dani Aschwanden <nimdanitro@gmail.com>
2024-07-02 09:25:10 +0200

    chore: Update yarn and webvitals (#259)
    * chore: Update yarn and webvitals
    
    * chore: Update node version
    
    * chore: Update github actions
    
    * chore: use explicit node version for asdf
    
    * chore: update action/cache
    
    * chore: Fix cache resolution in github workflow

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
+import reportWebVitals from './reportWebVitals';
 config.autoAddCss = false
 
 const container = document.getElementById('root');
@@ -18,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals(console.log);
+reportWebVitals(console.log);
